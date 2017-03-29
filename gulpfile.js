1e29a2b4b183e3e5c4fc7a31c39c32ed484033e0
broopimus <beckyliebing@gmail.com>
2017-03-28 20:02:30 -0600

    Add flexbox css

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var batch = require('gulp-batch');
+var postcss      = require('gulp-postcss');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
 
 gulp.task('autoprefixer', function () {
-    var postcss      = require('gulp-postcss');
-    var sourcemaps   = require('gulp-sourcemaps');
-    var autoprefixer = require('autoprefixer');
-
-    return gulp.src('style.css')
+return gulp.src('lolstyle.css')
         .pipe(sourcemaps.init())
         .pipe(postcss([ autoprefixer() ]))
         .pipe(sourcemaps.write('.'))
